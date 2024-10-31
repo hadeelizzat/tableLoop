93fdf25014f13bb724e61a8cf8e7b346297d4a2f
hadeelizzat <2205250127std@gtc.edu.ps>
2024-10-31 13:10:07 +0200

    first commit

---
+++ Assets/Scripts/MultiplicationTable.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiplicationTable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+           int Multiply (int no1,int no2){
+            return no1*no2;
+        }
+   
+        for(int i=1;i<=10;i++){
+            int n=5;
+            Debug.Log($"{n}x{i}= {Multiply(n,i)}");
+        }
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
